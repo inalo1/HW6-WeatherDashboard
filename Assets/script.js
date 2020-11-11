f346a809a5a26bbf25b2d0b7516a3a32e46f41a1
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2020-11-10 23:03:22 -0600

    JS: added code // was able to transfer inital content to webpage

--- Assets/script.js
+++ Assets/script.js
@@ -6,13 +6,40 @@
 // let cardUvEl = document.getElementById("card-uv");
 
 var weather = "Dallas";
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + weather + "2e55b9709282b2b6bdccfbaedeb0ba5f";
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + weather + "&&appid=919bda9b353ff6878f6c6628a095441a";
 
 $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
-    console.log(response)
-  
-});
+    console.log(response);
+    console.log(response.name);
+    // console.log(response.weather[0].icon);
+    console.log(response.main.temp);
+    let fahrenheitTemp = (response.main.temp - 273.15) * 1.80 + 32;
+      console.log(fahrenheitTemp);
+    console.log(response.main.humidity);
+    console.log(response.wind.speed);
+
+
+     // Code to transfer content to HTML
+     let $city = $("#city");
+     $city.text(response.name);
+
+    //  let $date = $("#date");
+    //  $date.text(response.name);
+    
+    //  let $icon = $("#icon");
+    //  $icon.text(response.weather[0].icon);
+
+     let $temp = $("#temp");
+     $temp.text("Temperature: " + fahrenheitTemp + " °F");
+
+     let $humid = $("#humid");
+     $humid.text("Humidity: " + response.main.humidity + "%");
+
+     let $wind = $("#wind");
+     $wind.text("Wind Speed: " + response.wind.speed + " MPH");
+     
+    });
 
