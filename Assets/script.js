bc24a222e25e3111fbfbdef42bfc8d35272b1a72
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2020-11-10 21:53:52 -0600

    INDEX: fixed page responsiveness

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,18 @@
-let cardCityEl = document.getElementById("card-city");
-let cardDateEl = document.getElementById("card-date");
-let cardTempEl = document.getElementById("card-temp");
-let cardHumidEl = document.getElementById("card-humi");
-let cardWindEl = document.getElementById("card-wind");
-let cardUvEl = document.getElementById("card-uv");
+// let cardCityEl = document.getElementById("card-city");
+// let cardDateEl = document.getElementById("card-date");
+// let cardTempEl = document.getElementById("card-temp");
+// let cardHumidEl = document.getElementById("card-humid");
+// let cardWindEl = document.getElementById("card-wind");
+// let cardUvEl = document.getElementById("card-uv");
 
-cardCityEl.textContent= {[
-    let city = 0;
+var weather = "Dallas";
+var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=" + weather + "2e55b9709282b2b6bdccfbaedeb0ba5f";
 
-]
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+    console.log(response)
+  
+});
 
-    
-}
