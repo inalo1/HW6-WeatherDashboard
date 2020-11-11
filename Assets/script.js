498ee12cfb38fd986b657bc5e63c23c9e9d88e00
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2020-11-10 18:34:42 -0600

    CSS: edited 5-Day Forecast card

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,14 @@
-z
+let cardCityEl = document.getElementById("card-city");
+let cardDateEl = document.getElementById("card-date");
+let cardTempEl = document.getElementById("card-temp");
+let cardHumidEl = document.getElementById("card-humi");
+let cardWindEl = document.getElementById("card-wind");
+let cardUvEl = document.getElementById("card-uv");
+
+cardCityEl.textContent= {[
+    let city = 0;
+
+]
+
+    
+}
