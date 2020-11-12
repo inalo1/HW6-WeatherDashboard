74d9cde095975b0f7f81a9e0593bd12828c8084c
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2020-11-12 13:41:08 -0600

    JS: added searchBtn for cities

--- Assets/script.js
+++ Assets/script.js
@@ -1,45 +1,49 @@
-// let cardCityEl = document.getElementById("card-city");
-// let cardDateEl = document.getElementById("card-date");
-// let cardTempEl = document.getElementById("card-temp");
-// let cardHumidEl = document.getElementById("card-humid");
-// let cardWindEl = document.getElementById("card-wind");
-// let cardUvEl = document.getElementById("card-uv");
+// let $weather = $("#mySearch")
 
-var weather = "Dallas";
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + weather + "&&appid=919bda9b353ff6878f6c6628a095441a";
+let weather = "";
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-    console.log(response);
-    console.log(response.name);
-    // console.log(response.weather[0].icon);
-    console.log(response.main.temp);
-    let fahrenheitTemp = (response.main.temp - 273.15) * 1.80 + 32;
+$("#searchBtn").on('click', function(){
+  weather = $("#searchTerm").val()
+  let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + weather + "&&appid=919bda9b353ff6878f6c6628a095441a";
+  weatherApiRequest(queryURL)
+  })
+
+  function weatherApiRequest(queryURL) {
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+      console.log(response.name);
+      // console.log(LocalDate.now());
+      // console.log(response.weather[0].icon);
+      console.log(response.main.temp);
+      let fahrenheitTemp = (response.main.temp - 273.15) * 1.80 + 32;
+      fahrenheitTemp = Math.floor(fahrenheitTemp);
       console.log(fahrenheitTemp);
-    console.log(response.main.humidity);
-    console.log(response.wind.speed);
-
-
-     // Code to transfer content to HTML
-     let $city = $("#city");
-     $city.text(response.name);
-
-    //  let $date = $("#date");
-    //  $date.text(response.name);
-    
-    //  let $icon = $("#icon");
-    //  $icon.text(response.weather[0].icon);
-
-     let $temp = $("#temp");
-     $temp.text("Temperature: " + fahrenheitTemp + " °F");
-
-     let $humid = $("#humid");
-     $humid.text("Humidity: " + response.main.humidity + "%");
-
-     let $wind = $("#wind");
-     $wind.text("Wind Speed: " + response.wind.speed + " MPH");
-     
-    });
-
+      console.log(response.main.humidity);
+      console.log(response.wind.speed);
+      
+      
+      // Code to transfer content to HTML
+      let $city = $("#city");
+      $city.text(response.name);
+      
+      //  let $date = $("#date");
+      //  let currentDate = System.out.println(LocalDate.now ());
+      //  $date.text(currentDate);
+      
+      let $icon = $("#icon");
+      $icon.text(response.weather[0].icon);
+      
+      let $temp = $("#temp");
+      $temp.text("Temperature: " + fahrenheitTemp + " °F");
+      
+      let $humid = $("#humid");
+      $humid.text("Humidity: " + response.main.humidity + "%");
+      
+      let $wind = $("#wind");
+      $wind.text("Wind Speed: " + response.wind.speed + " MPH");
+      
+      });
+  }
