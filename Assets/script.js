6a2c84fab71473b4b77f289f79c9008b1b2fc186
Ivy Gabrielle Nalo <nalo.ivielle13@gmail.com>
2021-03-24 01:01:44 -0500

    completely changed code - working

--- Assets/script.js
+++ Assets/script.js
@@ -1,49 +1,106 @@
-// let $weather = $("#mySearch")
+let apiKey = '919bda9b353ff6878f6c6628a095441a'
+let cityName = "";
+let curName = "";
+let curTemp = "";
+let curHumid = "";
+let curWind = "";
+let curUV = "";
+let curIcon = "";
+let cities = [];
 
-let weather = "";
+cityWeather();
+init();
+fiveDay();
 
-$("#searchBtn").on('click', function(){
-  weather = $("#searchTerm").val()
-  let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + weather + "&&appid=919bda9b353ff6878f6c6628a095441a";
-  weatherApiRequest(queryURL)
-  })
+function cityWeather() {
 
-  function weatherApiRequest(queryURL) {
+    let today = moment().format("MM/D/YY");
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`
     $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
-      console.log(response.name);
-      // console.log(LocalDate.now());
-      // console.log(response.weather[0].icon);
-      console.log(response.main.temp);
-      let fahrenheitTemp = (response.main.temp - 273.15) * 1.80 + 32;
-      fahrenheitTemp = Math.floor(fahrenheitTemp);
-      console.log(fahrenheitTemp);
-      console.log(response.main.humidity);
-      console.log(response.wind.speed);
-      
-      
-      // Code to transfer content to HTML
-      let $city = $("#city");
-      $city.text(response.name);
-      
-      //  let $date = $("#date");
-      //  let currentDate = System.out.println(LocalDate.now ());
-      //  $date.text(currentDate);
-      
-      let $icon = $("#icon");
-      $icon.text(response.weather[0].icon);
-      
-      let $temp = $("#temp");
-      $temp.text("Temperature: " + fahrenheitTemp + " °F");
-      
-      let $humid = $("#humid");
-      $humid.text("Humidity: " + response.main.humidity + "%");
-      
-      let $wind = $("#wind");
-      $wind.text("Wind Speed: " + response.wind.speed + " MPH");
-      
-      });
-  }
+        url: queryURL
+    }).then(response => {
+
+        $.ajax(`https://api.openweathermap.org/data/2.5/uvi?lat=${response.coord.lat}&lon=${response.coord.lon}&appid=${apiKey}`).then(uvResp => {
+            let uvData = `<p>UV Index: ${uvResp.value} </p>`
+            $("#mainData").append(uvData)
+        })
+        curName = response.name;
+        curTemp = response.main.temp;
+        curWind = response.wind.speed;
+        curHumid = response.main.humidity;
+        curIcon = response.weather[0].icon;
+        let mainData = `<h2>${curName} (${today})<img src="http://openweathermap.org/img/w/${curIcon}.png"></img></h2>
+                            <p>Temperature: ${curTemp} °F</p>
+                            <p>Humidity: ${curHumid}%</p>
+                            <p>Wind Speed: ${curWind} MPH</p>
+                           `
+        $("#mainData").append(mainData)
+
+    });
+};
+
+function fiveDay() {
+    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&cnt=0&units=imperial&appid=${apiKey}`
+    $.ajax({
+        url: queryURL
+    }).then(forecast => {
+        console.log(forecast)
+        for (let i = 0; i < 40; i += 8) {
+            foreDate = (forecast.list[i].dt_txt).slice(0, -8)
+            foreTemp = forecast.list[i].main.temp
+            foreIcon = forecast.list[i].weather[0].icon
+            foreHumid = forecast.list[i].main.humidity
+            let cardData = `<div class="card mx-1 border-light bg-primary rounded" style="width: 10rem;">
+                              <div class="card-body  text-white">
+                               <h5 class="card-title">${foreDate}</h5>
+                               <p class="card-text"><img src="http://openweathermap.org/img/w/${foreIcon}.png"></img></p>
+                               <p class="card-text">Temp: ${foreTemp} °F</p>
+                               <p class="card-text">Humidity: ${foreHumid}%</p>
+                              </div>
+                            </div>`
+            $("#forecast").append(cardData)
+        }
+    });
+}
+
+$("#search").on("click", function(event) {
+    event.preventDefault();
+    cityName = $("#city").val();
+    cities.push(cityName)
+
+    buildList();
+    $("#forecast").empty();
+    $("#mainData").empty();
+    cityWeather(cityName);
+    fiveDay(cityName);
+    localStorage.setItem("cities", JSON.stringify(cities));
+
+});
+
+function buildList() {
+    $("#history").empty();
+    for (let i = 0; i < cities.length; i++) {
+        const city = cities[i]
+        let cityList = `<li id="list" class="list-group-item list-group-item-action">${city}</li>`
+        $("#history").prepend(cityList)
+    }
+};
+
+function init() {
+    let citiesSaved = JSON.parse(localStorage.getItem("cities"));
+      if (citiesSaved !== null) {
+          cities = citiesSaved;
+      }
+    buildList();
+}
+
+$("li").on('click', function(event) {
+    event.preventDefault();
+    let histSearch = $(this).closest("#list").text()
+    console.log(histSearch)
+    cityName = histSearch
+    $("#forecast").empty();
+    $("#mainData").empty();
+    cityWeather(cityName);
+    fiveDay(cityName);
+})
